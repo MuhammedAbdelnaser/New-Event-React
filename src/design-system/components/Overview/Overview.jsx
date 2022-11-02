47e678711f0b6b98e350137a083df333700aa4b8
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2022-11-02 13:02:21 +0200

    install styled component and use it

---
+++ src/design-system/components/Overview/Overview.jsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Description, OverviewWrapper, Title } from './Styles';
+
+
+const Overview = ({ icon, title, description }) => {
+    return (
+        <OverviewWrapper>
+            <div>
+                <FontAwesomeIcon size='2x' color='#f2545f' icon={icon} />
+            </div>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+        </OverviewWrapper>
+    );
+};
+
+export default Overview;
