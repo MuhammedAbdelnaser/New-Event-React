b3d859778f24a5a1b9415fe4bb80f91dec29b7b0
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2022-11-27 16:06:06 +0200

    Modify Footer Component

--- src/design-system/pages/home/components/Footer/Footer.jsx
+++ src/design-system/pages/home/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FooterWrapper, Span } from './style'
+import { madeBy } from '../../../../config'
+import { FooterWrapper, Li, MadeBy, Span, Ul } from './style'
 
 export default function Footer() {
     return (
@@ -7,7 +8,14 @@ export default function Footer() {
             <div className='container'>
                 <div>
                     <Span>Copyright &copy; 2022 Your Company | Design:</Span>
+                    <MadeBy> {madeBy}</MadeBy>
                 </div>
+                <Ul>
+                    <Li></Li>
+                    <Li></Li>
+                    <Li></Li>
+                    <Li></Li>
+                </Ul>
             </div>
         </FooterWrapper>
     )
--- src/design-system/pages/home/components/Footer/style.jsx
+++ src/design-system/pages/home/components/Footer/style.jsx
@@ -10,3 +10,15 @@ export const Span = styled("span")`
     label: Span;
     color: ${theme.gray};
 `
+export const MadeBy = styled("span")`
+    label: MadeBy;
+    color: ${theme.primary};
+    font-weight: bold;
+    font-family: 'Open Sans';
+`
+export const Ul = styled("ul")`
+    label: Ul;
+`
+export const Li = styled("li")`
+    label: Li;
+`
